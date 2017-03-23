b54c9cda66375e2f53ca60916dbb97385a0d346a
My main account <sf2164229838@sina.com>
2017-03-23 13:47:24 +0800

    Add files via upload

---
+++ src/simulation/Particle.h
@@ -0,0 +1,24 @@
+#ifndef PARTICLE_H_
+#define PARTICLE_H_
+
+#include <vector>
+#include "StructProperty.h"
+
+struct Particle
+{
+	int type;
+	int life, ctype;
+	float x, y, vx, vy;
+	float temp;
+	float pavg[2];
+	int flags;
+	int tmp;
+	int tmp2, tmp3, tmp4;
+	unsigned int dcolour;
+	unsigned int cdcolour;
+	/** Returns a list of properties, their type and offset within the structure that can be changed
+	 by higher-level processes referring to them by name such as Lua or the property tool **/
+	static std::vector<StructProperty> GetProperties();
+};
+
+#endif
