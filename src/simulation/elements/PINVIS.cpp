cad9986b4cfc8074a81478aebbb1ee0427e26486
My main account <sf2164229838@sina.com>
2017-03-30 09:04:34 +0800

    Add files via upload

--- src/simulation/elements/PINVIS.cpp
+++ src/simulation/elements/PINVIS.cpp
@@ -41,8 +41,38 @@ Element_PINVIS::Element_PINVIS()
 	HighTemperature = ITH;
 	HighTemperatureTransition = NT;
 
-	Update = NULL;
+	Update = &Element_INVIS::update;
 	Graphics = &Element_INVIS::graphics;
+}
+
+//#TPT-Directive ElementHeader Element_PINVIS static int update(UPDATE_FUNC_ARGS)
+int Element_PINVIS::update(UPDATE_FUNC_ARGS)
+{
+	int r, rx, ry;
+	if (parts[i].life!=10)
+	{
+		if (parts[i].life>0)
+			parts[i].life--;
+	}
+	else
+	{
+		for (rx=-2; rx<3; rx++)
+			for (ry=-2; ry<3; ry++)
+				if (BOUNDS_CHECK && (rx || ry))
+				{
+					r = pmap[y+ry][x+rx];
+					if (!r)
+						continue;
+					if ((r&0xFF)==PT_PINVIS)
+					{
+						if (parts[r>>8].life<10&&parts[r>>8].life>0)
+							parts[i].life = 9;
+						else if (parts[r>>8].life==0)
+							parts[r>>8].life = 10;
+					}
+				}
+	}
+	return 0;
 }
 
 Element_PINVIS::~Element_PINVIS() {}
