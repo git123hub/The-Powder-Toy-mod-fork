8caa235451d866a9320122de7a0f144472c3fd1c
My main account <sf2164229838@sina.com>
2017-06-04 13:20:53 +0800

    Add files via upload

--- src/simulation/elements/PROT.cpp
+++ src/simulation/elements/PROT.cpp
@@ -96,6 +96,13 @@ int Element_PROT::update(UPDATE_FUNC_ARGS)
 		else change = 0.0f;
 		parts[under>>8].temp = restrict_flt(parts[under>>8].temp + change, MIN_TEMP, MAX_TEMP);
 		break;
+	case PT_E189:
+		if (parts[under>>8].life == 10 && parts[under>>8].temp > 1273.0f)
+		{
+			sim->kill_part(i);
+			return 1;
+		}
+		break;
 	case PT_NONE:
 		//slowly kill if it's not inside an element
 		if (parts[i].life)
